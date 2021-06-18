3509ebe74673bdcf7e7dbafbf8c41a195476aadb
ms <ms@kali.fr>
2021-06-18 14:44:55 +0200

    Coding Style

--- hello-1.c
+++ hello-1.c
@@ -1,17 +1,18 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/random.h> 
+
 int init_module(void)
 {
-	int k;
-	get_random_bytes(&k, sizeof(k));
-	printk(KERN_INFO "Hello world 1.\n");
+    int k;
+    get_random_bytes(&k, sizeof(k));
+    printk(KERN_INFO "Hello world 1.\n");
 
-	printk(KERN_INFO "BITE %d\n", k);
-	return 0;
+    printk(KERN_INFO "BITE %d\n", k);
+    return 0;
 }
 
 void cleanup_module(void)
 {
-	printk(KERN_INFO "Goodbye world 1.\n");
+    printk(KERN_INFO "Goodbye world 1.\n");
 }
