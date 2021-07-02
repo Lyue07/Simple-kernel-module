36b8225afe87821abc0151ddb6e9d19343796b0c
ms <ms@kali.fr>
2021-07-02 14:46:28 +0200

    Cleaning

--- chardev/chardev.c
+++ chardev/chardev.c
@@ -1,97 +1,67 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <asm/uaccess.h> /* for put_user */
-
-int init_module(void);
-void cleanup_module(void);
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
-
-#define SUCCESS 0
-#define DEVICE_NAME "chardev"
-#define BUF_LEN 80
-
-static int Major;
-static int Device_Open = 0;
-static char msg[BUF_LEN];
-static char *msg_Ptr;
+#include "chardev.h"
 
 static struct file_operations fops = {
-  .read = device_read,
-  .write = device_write,
-  .open = device_open,
-  .release = device_release
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release
 };
 
 int init_module(void)
 {
-  Major = register_chrdev(0, DEVICE_NAME, &fops);
+    Major = register_chrdev(0, DEVICE_NAME, &fops);
 
-  if (Major < 0) {
-    printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-    return Major;
-  }
+    if (Major < 0) {
+        printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+        return Major;
+    }
 
-  printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
-  printk(KERN_INFO "the driver, create a dev file with\n");
-  printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+    printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
 
-  return SUCCESS;
+    return SUCCESS;
 }
 
 void cleanup_module(void)
 {
-  unregister_chrdev(Major, DEVICE_NAME);
+    unregister_chrdev(Major, DEVICE_NAME);
 }
 
 static int device_open(struct inode *inode, struct file *filp)
 {
-  if (Device_Open)
-    return -EBUSY;
+    int rand;
+    get_random_bytes(&rand, sizeof(rand));
+    sprintf(msg, "%d", rand);
+    msg_Ptr = msg;
+    try_module_get(THIS_MODULE);
 
-  Device_Open++;
-  while (42)
-    printk(KERN_INFO "Hello world 1.\n");
-  msg_Ptr = msg;
-  try_module_get(THIS_MODULE);
-
-  return SUCCESS;
+    return SUCCESS;
 }
 
 static int device_release(struct inode *inode, struct file *filp)
 {
-  Device_Open--;
-
-  module_put(THIS_MODULE);
-
-  return SUCCESS;
+    module_put(THIS_MODULE);
+    return SUCCESS;
 }
 
-static ssize_t device_read(struct file *filp, 
-                           char *buffer,
-                           size_t length,
-                           loff_t *offset)
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset)
 {
-  int bytes_read = 0;
+    int bytes_read = 0;
 
-  if (*msg_Ptr == 0)
-    return 0;
+    if (*msg_Ptr == 0)
+        return 0;
 
-  while (length && *msg_Ptr) {
-    put_user(*(msg_Ptr++), buffer++);
-    length--;
-    bytes_read++;
-  }
+    while (length && *msg_Ptr) {
+        put_user(*(msg_Ptr++), buffer++);
+        length--;
+        bytes_read++;
+    }
 
-  return bytes_read;
+    return bytes_read;
 }
 
-static ssize_t
+    static ssize_t
 device_write(struct file *filp, const char *buf, size_t len, loff_t *off)
 {
-  printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
-  return -EINVAL;
+    printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
+    return -EINVAL;
 }
